7032791a7f711ac7bddcbcbc961e9150c1c17fc6
John Shewchuk <johnshew@users.noreply.github.com>
2022-01-03 18:20:16 -0800

    more logging

--- api/Test/index.ts
+++ api/Test/index.ts
@@ -2,7 +2,8 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    context.log(context, req);
+    context.log("context: " + JSON.stringify(context));
+    context.log("req: " + JSON.stringify(req));
     const user = req.body || {};
     console.log(user);
     context.res = {
